d2a23acde10f35f474efdbbe90f249efc84ca376
DJ Nrrd <djnrrd@gmail.com>
2021-05-15 11:23:25 +0100

    Updated with pro tip from Kapellosaur for calling venvs in shell scripts.

--- toggle_key_light.sh
+++ toggle_key_light.sh
@@ -3,10 +3,4 @@
 SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname "$SCRIPT")
 
-. $BASEDIR/venv/bin/activate
-
-if [ -n "$VIRTUAL_ENV" ]; then
-  python3 $BASEDIR/lights.py
-else
-  echo "Unable to detect Virtual Environment"
-fi
+$BASEDIR/venv/bin/python $BASEDIR/lights.py
