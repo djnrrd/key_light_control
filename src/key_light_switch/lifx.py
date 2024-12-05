f845249457fa75ce83c831d218382abb6db411e3
djnrrd <djnrrd@gmail.com>
2024-12-05 18:43:39 +0000

    Doc strings and default assignment

--- src/key_light_switch/lifx.py
+++ src/key_light_switch/lifx.py
@@ -5,22 +5,34 @@ from lifxlan.multizonelight import MultiZoneLight
 
 
 def get_key_light() -> Union[MultiZoneLight, None]:
+    """
+    Get the object representing the key light, currently hard coded to 'Back Strip')
+
+    :return: The light object or None
+    """
     lan = lifxlan.LifxLAN(6)
     retry = 0
+    light = None
     while retry < 3:
         try:
-            light = lan.get_device_by_name('Back Strip')
+            light = lan.get_device_by_name("Back Strip")
             if light:
                 retry = 3
             else:
                 retry += 1
         except WorkflowException as E:
-            light = None
             retry += 1
     return light
 
 
 def power_light(light: MultiZoneLight, state: str) -> bool:
+    """
+    Set the power state of the light object to either 'On' or 'Off'
+
+    :param light: The key light object
+    :param state: 'on' or 'off'
+    :return: Success state
+    """
     retry = 0
     ret_value = False
     while retry < 3:
@@ -34,6 +46,12 @@ def power_light(light: MultiZoneLight, state: str) -> bool:
 
 
 def set_blue_daylight(light: MultiZoneLight) -> bool:
+    """
+    Set the colour and brightness of the key light object to Blue Daylight
+
+    :param light: The key light object
+    :return: Success state
+    """
     retry = 0
     ret_value = False
     while retry < 3:
@@ -47,15 +65,21 @@ def set_blue_daylight(light: MultiZoneLight) -> bool:
 
 
 def toggle_light(light: MultiZoneLight) -> bool:
+    """
+    Toggle the power status of the key light object between 'On' and 'Off'
+
+    :param light: The key light object
+    :return: Success state
+    """
     retry = 0
     ret_value = False
     while retry < 3:
         try:
             state = light.get_power()
             if state > 0:
-                result = power_light(light, 'off')
+                result = power_light(light, "off")
             else:
-                result = power_light(light, 'on')
+                result = power_light(light, "on")
             if result:
                 retry = 3
                 ret_value = True
